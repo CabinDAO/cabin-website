6fb28a67a08dc0e00c4b66bcd23cbe13b708a1a6
DugDaniels <cdouglasd@gmail.com>
2022-01-14 17:09:14 -0500

    New footer

---
+++ pages/products/daocamp/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../../components/Layout";
+import UnderConstruction from "../../../components/UnderConstruction";
+
+const DaoCampPage = () => {
+  return (
+    <Layout>
+      <UnderConstruction />
+    </Layout>
+  );
+};
+
+export default DaoCampPage;
