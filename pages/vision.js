337f599da58a9eba6f1e25f450c2dab1535f0a07
DugDaniels <cdouglasd@gmail.com>
2022-01-27 13:30:10 -0500

    More content

--- pages/vision.js
+++ pages/vision.js
@@ -1,8 +1,11 @@
 import Layout from "../components/Layout";
 import Wrapper from "../components/Wrapper";
+import CallToAction from '../components/CallToAction';
 import visionImage from "../public/visionImage.jpg";
+import visionStepImage from "../public/visionStepImage.jpg";
 import Image from 'next/image'
 import { styled } from "../stitches.config";
+import { HomeIcon, Share1Icon, RocketIcon } from '@radix-ui/react-icons'
 
 const Intro = styled("section", {
   pt: "$8",
@@ -79,6 +82,92 @@ const Followup = styled("div", {
   },
 });
 
+const FirstStep = styled("div", {
+  bc: "$forest",
+  color: "$sand",
+  py: "$12",
+  mb: "$24",
+  "@md": {
+    py: "$32",
+    mb: "$32",
+  },
+});
+
+const Step = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  mb: "$12",
+  h2: { 
+    flex: "1",
+    fontFamily: "$mono",
+    fontSize: "$xxl",
+    fontWeight: 600,
+    lineHeight: 1.6,
+  },
+  "& p": {
+    flex: "1",
+  },
+  "@md": {
+    mb: "$24",
+    flexDirection: "row",
+    h2: {
+      fontSize: "$xxxl",
+      maxWidth: "40rem",
+      mt: "$4",
+      pr: "$24",
+    },
+  },
+});
+
+const Meaning = styled("div", {
+  pt: "$12",
+  h3: { 
+    fontFamily: "$mono",
+    fontSize: "$xl",
+    fontWeight: 600,
+    lineHeight: 1.6,
+  },
+  "> div": {
+    display: "flex",
+    flexDirection: "column",
+  },
+  "@md": {
+    pt: "$24",
+    h3: {
+      fontSize: "$xxl",
+      textAlign: "center",
+    },
+    "> div": {
+      flexDirection: "row",
+    },
+  },
+});
+
+const MeaningItem = styled("div", {
+  pt: "$10",
+  div: {
+    height: "$12",
+    width: "$12",
+    border: "1px solid $sprout",
+    br: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  svg: {
+    height: "$5",
+    width: "$5",
+    color: "$sprout",
+  },
+  "@md": {
+    pt: "$12",
+    mr: "$20",
+    "&:last-child": {
+      mr: 0,
+    },
+  },
+});
+
 const VisionPage = () => {
   return (
     <Layout>
@@ -109,6 +198,55 @@ const VisionPage = () => {
           </Followup>
         </Wrapper>
       </Intro>
+      <FirstStep>
+        <Wrapper>
+          <Step>
+            <h2>First step: building bridges and becoming an embassy</h2>
+            <p>
+              DAOs today are somewhat like disparate nations, all working toward goals of decentralization, innovation and new forms of collaboration, but operating in distinct silos. As a first step toward building a global decentralized city, Cabin will build bridges both online and IRL for DAO operators to collaborate and further their individual projects.
+            </p>
+          </Step>
+          <Frame>
+            <Image
+              src={visionStepImage}
+              alt="Sime trees backlit by a sunrise"
+              layout="responsive"
+              width={984}
+              height={340}
+            />
+          </Frame>
+          <Meaning>
+            <h3>What it means to be an embassy for DAOs</h3>
+            <div>
+              <MeaningItem>
+                <div>
+                  <HomeIcon />
+                </div>
+                <p>
+                  Establishing embassies and ambassadorships to foster DAO to DAO collaboration
+                </p>
+              </MeaningItem>
+              <MeaningItem>
+                <div>
+                  <Share1Icon />
+                </div>
+                <p>
+                  Hosting conferences, retreats, and events to bring DAO operators together IRL at Cabin nodes
+                </p>
+              </MeaningItem>
+              <MeaningItem>
+                <div>
+                  <RocketIcon />
+                </div>
+                <p>
+                  Building products and tooling that support the development of the DAO ecosystem
+                </p>
+              </MeaningItem>
+            </div>
+          </Meaning>
+        </Wrapper>
+      </FirstStep>
+      <CallToAction />
     </Layout>
   );
 };
