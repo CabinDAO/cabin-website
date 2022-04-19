42ba25d4629ed2d252154cf43ededf1a7d720306
Zakk Fleischmann <zakk@thinknimble.com>
2022-04-19 18:24:56 -0500

    fix spelling issue in Contribute section of homepage

--- components/home/Contribute.js
+++ components/home/Contribute.js
@@ -1,18 +1,17 @@
-import Image from 'next/image'
-import bgImage from '../../public/water.jpg';
-import featureImage from '../../public/guestsTable.jpg';
-import SideImage from '../SideImage';
-
+import Image from "next/image";
+import bgImage from "../../public/water.jpg";
+import featureImage from "../../public/guestsTable.jpg";
+import SideImage from "../SideImage";
 
 export default function Reality() {
+  const title = "Contribute, grow, and recharge";
+  const content =
+    "Web3 can feel like wandering through a dark forest without a map. We are a cabin in the woods where you can stop in for a drink and a warm bed, in the metaverse and IRL. Cross paths with other creators and DAOs to share learnings, build mutual financial incentives and develop the culture of web3.";
+  const featureImageAlt = "People talking around a table";
+  const bgImageAlt = "Texas hill country scenery";
 
-  const title = 'Contribute, grow, and recharge';
-  const content = 'Web3 can feel like wandering through a dark forest without a map. We are a cabin in the woods where you can stop in for a drink and a warm bed, in the metaverse and IRL. Cross paths with other creators and DAOs to share learnings, build mutal financial incentives and develop the culture of web3.';
-  const featureImageAlt = 'People talking around a table';
-  const bgImageAlt = 'Texas hill country scenery';
-  
   return (
-    <SideImage 
+    <SideImage
       title={title}
       content={content}
       bgImage={bgImage}
@@ -21,5 +20,5 @@ export default function Reality() {
       featureImageAlt={featureImageAlt}
       reversed
     />
-  )
+  );
 }
