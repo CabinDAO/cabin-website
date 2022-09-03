07aa1eca0643bf6ff810a43f7a0a738702e41a7a
Zakk <zakk@hawthorne.io>
2022-09-03 10:05:26 -0500

    scaffold out neighborhood detail

---
+++ pages/neighborhoods/[:id].tsx
@@ -0,0 +1,21 @@
+import { Heading } from "@cabindao/topo"
+import { PrimaryHero, SimpleCallToAction, NeighborhoodDetail } from "@templates"
+
+export default function NeighborhoodDetail() {
+  return (
+    <>
+      {/* Gallery Hero */}
+      {/* Neighborhood details */}
+      {/* Colive micro CTA */}
+      {/* Lore section */}
+      {/* Amenities List */}
+      {/* Dark themed call to action section */}
+      <SimpleCallToAction
+        title="Join us at Neighborhood 0 and become part of the lore"
+        cta={{ href: "/neighborhoods/become-a-neighborhood", label: "Apply to Colive" }}
+        theme="dark"
+      />
+    </>
+  )
+}
+
